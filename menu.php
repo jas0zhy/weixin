151793f2e6248facbba0a8c0c0ce1a6b7a06f386
jas <942196593@qq.com>
2018-09-21 13:48:30 +0800

    ‘menu’

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php
 
-$APPID="wxdxxxxxxxxxxxxxxx";
-$APPSECRET="96xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$APPID="wxcd79416fde69d146";
+$APPSECRET="9e70d0e9efdcf88290482a05159583da";
 
 $TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$APPID."&secret=".$APPSECRET;
 
