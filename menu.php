32a5c535d4680afce977e6bfbfed3b76875247c3
jas <942196593@qq.com>
2018-09-21 13:45:36 +0800

    ‘menu’

--- menu.php
+++ menu.php
@@ -1,12 +1,15 @@
 <?php
 
-$APPID="wxcd79416fde69d146";
-$APPSECRET="9e70d0e9efdcf88290482a05159583da";
+$APPID="wxdxxxxxxxxxxxxxxx";
+$APPSECRET="96xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 $TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$APPID."&secret=".$APPSECRET;
 
 $json=file_get_contents($TOKEN_URL);
 $result=json_decode($json,true);
-print_r($result);
+
+$ACC_TOKEN=$result['access_token'];
+
+echo $ACC_TOKEN;
 
 ?>
